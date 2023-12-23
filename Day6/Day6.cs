e4cadef515ea3a9a530f3f0b08cc4ca23613aecc
Viktoriia Chykrii <vika.chikrii@gmail.com>
2023-12-23 03:16:15 +0100

    Solved Day 6 part2

--- Day6/Day6.cs
+++ Day6/Day6.cs
@@ -16,15 +16,15 @@ Distance:  9  40  200
             var times = processedInput[0].Split(':').LastOrDefault()?.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
             var distances = processedInput[1].Split(':').LastOrDefault()?.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
 
-            int expectedResult = 288;
-            int result = 0;
+            long expectedResult = 288;
+            long result = 0;
 
             for (int i = 0; i < times?.Count; i++)
             {
-                int time = int.Parse(times[i]);
-                int distance = int.Parse(distances[i]);
+                long time = long.Parse(times[i]);
+                long distance = long.Parse(distances[i]);
 
-                int countWaysToBeatRecord = GetNumberOfWaysToBeatRecord(time, distance);
+                long countWaysToBeatRecord = GetNumberOfWaysToBeatRecord(time, distance);
                 result = result == 0 ? result + countWaysToBeatRecord : result * countWaysToBeatRecord;
             }
 
@@ -40,28 +40,60 @@ Distance:  9  40  200
             var times = processedInput[0].Split(':').LastOrDefault()?.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
             var distances = processedInput[1].Split(':').LastOrDefault()?.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
 
-            int result = 0;
+            long result = 0;
 
             for (int i = 0; i < times?.Count; i++)
             {
-                int time = int.Parse(times[i]);
-                int distance = int.Parse(distances[i]);
+                long time = long.Parse(times[i]);
+                long distance = long.Parse(distances[i]);
 
-                int countWaysToBeatRecord = GetNumberOfWaysToBeatRecord(time, distance);
+                long countWaysToBeatRecord = GetNumberOfWaysToBeatRecord(time, distance);
                 result = result == 0 ? result + countWaysToBeatRecord : result * countWaysToBeatRecord;
             }
 
             Console.WriteLine($"Multiplication number of ways to beat the record is: {result}");
         }
 
-        public static int GetNumberOfWaysToBeatRecord(int time, int distance)
+        [Test]
+        public void Part2_test_input_should_return_correct_multiplication_of_number_of_ways_to_beat_the_record()
         {
-            int countWaysToBeatRecord = 0;
+            var input = @"
+Time:      7  15   30
+Distance:  9  40  200
+";
+            var processedInput = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).ToList();
+            long time = long.Parse(processedInput[0].Split(':').LastOrDefault()?.Trim().Replace(" ", ""));
+            long distance = long.Parse(processedInput[1].Split(':').LastOrDefault()?.Trim().Replace(" ", ""));
+
+            long expectedResult = 71503;
+
+            long countWaysToBeatRecord = GetNumberOfWaysToBeatRecord(time, distance);
+
+            Assert.That(countWaysToBeatRecord, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void Part2_my_input_should_return_multiplication_of_number_of_ways_to_beat_the_record()
+        {
+            var path = $"6-wait-for-it.txt";
+            var processedInput = File.ReadAllLines(path, Encoding.UTF8).ToList();
+
+            long time = long.Parse(processedInput[0].Split(':').LastOrDefault()?.Trim().Replace(" ", ""));
+            long distance = long.Parse(processedInput[1].Split(':').LastOrDefault()?.Trim().Replace(" ", ""));
+
+            long countWaysToBeatRecord = GetNumberOfWaysToBeatRecord(time, distance);
+
+            Console.WriteLine($"Multiplication number of ways to beat the record is: {countWaysToBeatRecord}");
+        }
+
+        public static long GetNumberOfWaysToBeatRecord(long time, long distance)
+        {
+            long countWaysToBeatRecord = 0;
 
             for (var holdTheButtonTime = 0; holdTheButtonTime <= time; holdTheButtonTime++)
             {
-                int speed = 1 * holdTheButtonTime;
-                int timeLeft = time - holdTheButtonTime;
+                long speed = 1 * holdTheButtonTime;
+                long timeLeft = time - holdTheButtonTime;
 
                 countWaysToBeatRecord = speed * timeLeft > distance 
                     ? countWaysToBeatRecord + 1 
